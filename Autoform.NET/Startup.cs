863182729ed6b6f6e37d41630aead5ccb604de63
mig1023 <mig1023@yandex.ru>
2020-09-26 19:10:02 +0300

    asp.net + react.js start

---
+++ Autoform.NET/Startup.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using React.AspNet;
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+
+namespace Autoform.NET
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName).AddChakraCore();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseReact(config => { });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ Autoform.NET/wwwroot/js/autoform.jsx
@@ -0,0 +1,9 @@
+﻿class Hello extends React.Component {
+    render() {
+        return <h1 class="panel-heading text-center">Autoform.NET</h1>;
+    }
+}
+ReactDOM.render(
+    <Hello />,
+    document.getElementById("content")
+);
